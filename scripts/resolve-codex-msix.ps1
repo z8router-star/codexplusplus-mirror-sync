1f5085aa1a02251a53854bba5e253b429a42757d
z8router-star <z8router-star@users.noreply.github.com>
2026-08-29 06:05:40 +0800

    fix: resolve official desktop package and Gitee releases

--- scripts/resolve-codex-msix.ps1
+++ scripts/resolve-codex-msix.ps1
@@ -6,95 +6,41 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 
-function Get-DeviceAttributes {
-    $version = [Environment]::OSVersion.Version.ToString()
-    $osArchitecture = if ($Architecture -eq 'arm64') { 'ARM64' } else { 'AMD64' }
-    return "BranchReadinessLevel=CB;CurrentBranch=rs_prerelease;InstallLanguage=en-US;OSUILocale=en-US;InstallationType=Client;FlightingBranchName=external;OSSkuId=48;FlightContent=Branch;App=WU;AppVer=$version;OSArchitecture=$osArchitecture;UpdateManagementGroup=2;IsFlightingEnabled=1;TelemetryLevel=3;OSVersion=$version;DeviceFamily=Windows.Desktop;"
-}
+# Reuse the public Microsoft Store resolver logic to obtain a time-limited
+# Microsoft CDN URL; only the resulting official URL is consumed below.
+$archive = Join-Path ([IO.Path]::GetTempPath()) 'z8-get-msstoreinstaller.nupkg'
+$extract = Join-Path ([IO.Path]::GetTempPath()) 'z8-get-msstoreinstaller'
+Invoke-WebRequest -Uri 'https://www.powershellgallery.com/api/v2/package/Get-MSStoreInstaller/1.0.1' -OutFile $archive
+if (Test-Path -LiteralPath $extract) { Remove-Item -LiteralPath $extract -Recurse -Force }
+Expand-Archive -LiteralPath $archive -DestinationPath $extract -Force
+$source = Get-Content -Raw -LiteralPath (Join-Path $extract 'Get-MSStoreInstaller.ps1')
+$start = $source.IndexOf('#region Download Functions')
+$end = $source.LastIndexOf('#endregion')
+if ($start -lt 0 -or $end -le $start) { throw 'Unable to load the Microsoft Store resolver functions.' }
+Invoke-Expression $source.Substring($start, $end - $start + 10)
 
-function Invoke-StoreSoap {
-    param([string]$Uri, [string]$Body)
-    Invoke-RestMethod -Method Post -Uri $Uri -Body $Body -Headers @{
-        'Content-Type' = 'application/soap+xml; charset=utf-8'
+$items = @(Get-StoreURLS -ProductNumber $ProductId -Architecture $Architecture)
+$item = $items | Where-Object { $_.FileName -match "OpenAI\.Codex_(?<version>\d+\.\d+\.\d+\.\d+)_${Architecture}_" } | Select-Object -First 1
+if ($null -eq $item) { throw "Microsoft Store returned no OpenAI.Codex $Architecture MSIX." }
+
+$candidates = foreach ($candidate in @($item.URLS)) {
+    try {
+        $head = Invoke-WebRequest -Uri $candidate -Method Head
+        $length = [long]$head.Headers['Content-Length']
+        [pscustomobject]@{ Url = [string]$candidate; Size = $length }
+    } catch {
+        [pscustomobject]@{ Url = [string]$candidate; Size = 0L }
     }
 }
-
-$serviceUri = 'https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx'
-$securedUri = "$serviceUri/secured"
-$productUri = "https://storeedgefd.dsx.mp.microsoft.com/v9.0/products/${ProductId}?market=US&locale=en-us&deviceFamily=Windows.Desktop"
-$product = Invoke-RestMethod -Method Get -Uri $productUri
-$fulfillment = $product.Payload.Skus[0].FulfillmentData | ConvertFrom-Json
-$categoryId = [string]$fulfillment.WuCategoryId
-if ([string]::IsNullOrWhiteSpace($categoryId)) {
-    throw "Microsoft Store returned no fulfillment category for $ProductId."
-}
-
-$now = [DateTime]::UtcNow
-$created = $now.ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
-$cookieExpires = $now.AddDays(27).ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
-$syncExpires = $now.AddMinutes(5).ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
-$lastChange = $now.AddYears(-2).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ')
-$currentTime = $now.AddMilliseconds(7).ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
-$device = Get-DeviceAttributes
-
-$cookieRequest = @"
-<Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.w3.org/2003/05/soap-envelope"><Header><Action d3p1:mustUnderstand="1" xmlns:d3p1="http://www.w3.org/2003/05/soap-envelope" xmlns="http://www.w3.org/2005/08/addressing">http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/GetCookie</Action><MessageID xmlns="http://www.w3.org/2005/08/addressing">urn:uuid:$([guid]::NewGuid())</MessageID><To d3p1:mustUnderstand="1" xmlns:d3p1="http://www.w3.org/2003/05/soap-envelope" xmlns="http://www.w3.org/2005/08/addressing">$serviceUri</To><Security d3p1:mustUnderstand="1" xmlns:d3p1="http://www.w3.org/2003/05/soap-envelope" xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"><Timestamp xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><Created>$created</Created><Expires>$cookieExpires</Expires></Timestamp><WindowsUpdateTicketsToken d4p1:id="ClientMSA" xmlns:d4p1="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" xmlns="http://schemas.microsoft.com/msus/2014/10/WindowsUpdateAuthorization"><TicketType Name="MSA" Version="1.0" Policy="MBI_SSL"><User /></TicketType></WindowsUpdateTicketsToken></Security></Header><Body><GetCookie xmlns="http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService"><oldCookie /><lastChange>$lastChange</lastChange><currentTime>$currentTime</currentTime><protocolVersion>1.40</protocolVersion></GetCookie></Body></Envelope>
-"@
-$cookieResponse = Invoke-StoreSoap -Uri $serviceUri -Body $cookieRequest
-$cookieNode = $cookieResponse.GetElementsByTagName('EncryptedData')[0]
-if ($null -eq $cookieNode) { throw 'Microsoft update service returned no cookie.' }
-$cookie = $cookieNode.FirstChild.Data
-$cookieRelatesTo = [string]$cookieResponse.GetElementsByTagName('RelatesTo')[0].InnerText
-$cookieExpirationNode = $cookieResponse.GetElementsByTagName('Expiration') | Select-Object -First 1
-$cookieExpiration = if ($null -ne $cookieExpirationNode) { [string]$cookieExpirationNode.InnerText } else { $cookieExpires }
-
-$syncRequest = @"
-<s:Envelope xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:s="http://www.w3.org/2003/05/soap-envelope"><s:Header><a:Action s:mustUnderstand="1">http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/SyncUpdates</a:Action><a:MessageID>urn:uuid:$([guid]::NewGuid())</a:MessageID><a:To s:mustUnderstand="1">$serviceUri</a:To><o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"><Timestamp xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wssecurity-utility-1.0.xsd"><Created>$created</Created><Expires>$syncExpires</Expires></Timestamp><wuws:WindowsUpdateTicketsToken wsu:id="ClientMSA" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" xmlns:wuws="http://schemas.microsoft.com/msus/2014/10/WindowsUpdateAuthorization"><TicketType Name="MSA" Version="1.0" Policy="MBI_SSL">Retail</TicketType></wuws:WindowsUpdateTicketsToken></o:Security></s:Header><s:Body><SyncUpdates xmlns="http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService"><cookie><Expiration>2045-03-11T02:02:48Z</Expiration><EncryptedData>$cookie</EncryptedData></cookie><parameters><ExpressQuery>false</ExpressQuery><InstalledNonLeafUpdateIDs><int>1</int><int>2</int><int>11</int><int>23110993</int></InstalledNonLeafUpdateIDs><OtherCachedUpdateIDs /><SkipSoftwareSync>false</SkipSoftwareSync><NeedTwoGroupOutOfScopeUpdates>true</NeedTwoGroupOutOfScopeUpdates><FilterAppCategoryIds><CategoryIdentifier><Id>$categoryId</Id></CategoryIdentifier></FilterAppCategoryIds><TreatAppCategoryIdsAsInstalled>true</TreatAppCategoryIdsAsInstalled><AlsoPerformRegularSync>false</AlsoPerformRegularSync><ComputerSpec /><ExtendedUpdateInfoParameters><XmlUpdateFragmentTypes><XmlUpdateFragmentType>Extended</XmlUpdateFragmentType></XmlUpdateFragmentTypes><Locales><string>en-US</string><string>en</string></Locales></ExtendedUpdateInfoParameters><ClientPreferredLanguages><string>en-US</string></ClientPreferredLanguages><ProductsParameters><SyncCurrentVersionOnly>false</SyncCurrentVersionOnly><DeviceAttributes>$device</DeviceAttributes><CallerAttributes>Interactive=1;IsSeeker=0;</CallerAttributes><Products /></ProductsParameters></parameters></SyncUpdates></s:Body></s:Envelope>
-"@
-$syncRequest = $syncRequest -replace '(?=<a:To s:mustUnderstand)', "<a:RelatesTo>urn:uuid:$cookieRelatesTo</a:RelatesTo>"
-$syncRequest = $syncRequest -replace '<TicketType Name="MSA" Version="1.0" Policy="MBI_SSL">Retail</TicketType>', '<TicketType Name="MSA" Version="1.0" Policy="MBI_SSL"></TicketType>'
-$syncRequest = $syncRequest -replace '<Expiration>2045-03-11T02:02:48Z</Expiration>', "<Expiration>$cookieExpiration</Expiration>"
-$syncResponse = Invoke-StoreSoap -Uri $serviceUri -Body $syncRequest
-[xml]$syncXml = $syncResponse.InnerXml.ToString().Replace('&lt;', '<').Replace('&gt;', '>')
-$updates = @{}
-foreach ($fragment in $syncXml.GetElementsByTagName('SecuredFragment')) {
-    $id = $fragment.ParentNode.ParentNode.ParentNode.GetElementsByTagName('ID')[0].FirstChild.Value
-    $identity = $fragment.ParentNode.ParentNode.FirstChild
-    $updates[$id] = [pscustomobject]@{ UpdateId = $identity.UpdateID; Revision = $identity.RevisionNumber }
-}
-
-$packages = foreach ($filesNode in $syncXml.GetElementsByTagName('Files')) {
-    $file = $filesNode.FirstChild
-    if ($null -eq $file) { continue }
-    $id = $filesNode.ParentNode.ParentNode.GetElementsByTagName('ID')[0].FirstChild.Value
-    if (-not $updates.ContainsKey($id)) { continue }
-    $fileName = $file.Attributes['InstallerSpecificIdentifier'].Value + '_' + $file.Attributes['FileName'].Value
-    if ($fileName -notmatch "^OpenAI\.Codex_(?<version>\d+\.\d+\.\d+\.\d+)_${Architecture}__.*\.(msix|msixbundle)$") { continue }
-    [pscustomobject]@{
-        fileName = $fileName
-        version = [version]$Matches.version
-        digest = $file.Attributes['Digest'].Value
-        size = [long]$file.Attributes['Size'].Value
-        updateId = $updates[$id].UpdateId
-        revision = $updates[$id].Revision
-    }
-}
-$package = $packages | Sort-Object version -Descending | Select-Object -First 1
-if ($null -eq $package) { throw "Microsoft Store returned no OpenAI.Codex $Architecture MSIX." }
-
-$fileRequest = @"
-<s:Envelope xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:s="http://www.w3.org/2003/05/soap-envelope"><s:Header><a:Action s:mustUnderstand="1">http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/GetExtendedUpdateInfo2</a:Action><a:MessageID>urn:uuid:$([guid]::NewGuid())</a:MessageID><a:To s:mustUnderstand="1">$securedUri</a:To><o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"><Timestamp xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wssecurity-utility-1.0.xsd"><Created>$created</Created><Expires>$syncExpires</Expires></Timestamp><wuws:WindowsUpdateTicketsToken wsu:id="ClientMSA" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wssecurity-utility-1.0.xsd" xmlns:wuws="http://schemas.microsoft.com/msus/2014/10/WindowsUpdateAuthorization"><TicketType Name="MSA" Version="1.0" Policy="MBI_SSL">Retail</TicketType></wuws:WindowsUpdateTicketsToken></o:Security></s:Header><s:Body><GetExtendedUpdateInfo2 xmlns="http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService"><updateIDs><UpdateIdentity><UpdateID>$($package.updateId)</UpdateID><RevisionNumber>$($package.revision)</RevisionNumber></UpdateIdentity></updateIDs><infoTypes><XmlUpdateFragmentType>FileUrl</XmlUpdateFragmentType><XmlUpdateFragmentType>FileDecryption</XmlUpdateFragmentType></infoTypes><deviceAttributes>$device</deviceAttributes></GetExtendedUpdateInfo2></s:Body></s:Envelope>
-"@
-$fileResponse = Invoke-StoreSoap -Uri $securedUri -Body $fileRequest
-$location = $fileResponse.GetElementsByTagName('FileLocation') | Where-Object { $_.FileDigest -eq $package.digest } | Select-Object -First 1
-if ($null -eq $location -or [string]::IsNullOrWhiteSpace($location.Url)) {
-    throw 'Microsoft update service returned no matching MSIX URL.'
-}
+$selected = $candidates | Where-Object { $_.Url -match '\?P1=' } | Select-Object -First 1
+if ($null -eq $selected) { $selected = $candidates | Sort-Object Size -Descending | Select-Object -First 1 }
+if ($null -eq $selected -or [string]::IsNullOrWhiteSpace($selected.Url)) { throw 'Microsoft Store returned no downloadable MSIX URL.' }
 
 [pscustomobject]@{
-    fileName = $package.fileName
-    version = $package.version.ToString()
-    size = $package.size
-    url = [string]$location.Url
+    fileName = [string]$item.FileName
+    version = [regex]::Match([string]$item.FileName, 'OpenAI\.Codex_(?<version>\d+\.\d+\.\d+\.\d+)_').Groups['version'].Value
+    size = [long]$selected.Size
+    url = [string]$selected.Url
 } | ConvertTo-Json -Compress
